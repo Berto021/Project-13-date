c19e2bc30daa035f68d1460ae822647bbe9a7943
Humberto <humbertolima108@gmail.com>
2023-06-28 01:08:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let button = document.querySelector('#no');
+let height = window.innerHeight - 650;
+let width = window.innerWidth - 650;
+const cat = document.querySelector('.background-sad-cat')
+const box = document.querySelector('.box')
+const textBox = document.querySelector('.text-container')
+
+
+button.addEventListener('mouseover', function () {
+    button.style.position = "absolute"
+    button.style.top = Math.random() * height + "px";
+    button.style.left = Math.random() * width + "px";
+})
+button.addEventListener('click', function(){
+    box.style.display = 'none'
+    cat.style.display = 'flex'
+    textBox.style.display = 'none'
+    
+})
+
